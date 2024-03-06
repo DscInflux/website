4085b910de88cb4a470a4abd402f961d6c9f9e5a
Maya <foenx0315@gmail.com>
2024-03-06 12:38:58 +0530

    Enabled to fetch team members directly from the API.
    Enabled to fetch team members directly from the API. Also added env template and some fixes to apiHandler

--- src/utils/apiHandler.js
+++ src/utils/apiHandler.js
@@ -1,6 +1,6 @@
 module.exports.request = async (path, method, data, accessToken = null) => {
   try {
-    const API_URL = "https://api.dscinflux.xyz/v1";
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;    
     const url = `${API_URL}${path}`;
 
     const isLocalStorage = typeof localStorage !== "undefined";
